70d8bd00fedf13bc0985369b742188a2814069a0
ALVESZ2 <evertonporalves73@gmail.com>
2025-07-08 20:05:57 -0300

    adicionando a base do javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiKeyInput = document.getElementById('apiKey'); //document.querySelector('#apiKey')
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getlementsById('askBUtton');
+const form = document.getElementById('form');
+const iaResponse = document.getElementById('iaResponse');
+
+const enviarForm =() => {
+
+}
+
+form.addEventListener('submit', enviarForm) //"adicionar um ouvidor de eventos"
